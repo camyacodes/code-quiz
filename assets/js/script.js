2c4e01c1cfa72bbb7c4e50415d5e243eb997ffc9
camyacodes <camyacodes@gmail.com>
2021-11-10 12:17:29 -0500

    added quiz function that dynamically changes html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,28 @@
-var startBtn = document.querySelector ("#start-btn")
-var timerEl = document.getElementById("timer")
+var startBtn = document.querySelector("#start-btn");
+var timerEl = document.getElementById("timer");
+var answerChoices = document.querySelector(".answers-choices");
+var instructions = document.querySelector("#instructions");
+var questionEl = document.querySelector("#Question");
 
 function timer() {
-    var timeLeft = 75;
-    var timeInterval = setInterval(function(){
-        if (timeLeft > 0) {
-            timerEl.textContent = "Time: " + timeLeft;
-            timeLeft --;
-        } else { 
-            timerEl.textContent = "0"
-            clearInterval(timeInterval)
-        }
-    },1000)
+  quiz();
+  var timeLeft = 75;
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 0) {
+      timerEl.textContent = "Time: " + timeLeft;
+      timeLeft--;
+    } else {
+      timerEl.textContent = "0";
+      clearInterval(timeInterval);
+    }
+  }, 1000);
 }
 
-startBtn.addEventListener ("click", timer)
+function quiz() {
+  answerChoices.hidden = false;
+  startBtn.hidden = true;
+  instructions.hidden = true;
+  questionEl.textContent = "Question 1";
+}
+
+startBtn.addEventListener("click", timer);
