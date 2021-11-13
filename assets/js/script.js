cb464bf0256bfd9bc41e7f213e070724cc040fe1
camyacodes <camyacodes@gmail.com>
2021-11-12 21:34:34 -0500

    added start quiz function to show first question w/ choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,40 @@
 var startBtn = document.querySelector("#start-btn");
 var timerEl = document.getElementById("timer");
-var answerChoices = document.querySelector(".answers-choices");
+var answerChoicesContainer = document.querySelector(".choices");
+var choices = Array.from(document.querySelectorAll(".choice"));
 var instructions = document.querySelector("#instructions");
 var questionEl = document.querySelector("#Question");
+// var buttonContainer = document.querySelector(".answers-choices");
+// var button = document.querySelectorAll("button");
+
+var currentQuestion = {};
+
+var questions = [
+  {
+    question: "What is 10/2?",
+    choice1: "3",
+    choice2: "5",
+    choice3: "115",
+    answer: 1,
+  },
+  {
+    question: "What is 20/2?",
+    choice1: "3",
+    choice2: "10",
+    choice3: "115",
+    answer: 2,
+  },
+  {
+    question: "What is 10/1?",
+    choice1: "10",
+    choice2: "5",
+    choice3: "115",
+    answer: 1,
+  },
+];
+
+var availableQuestions = [...questions];
+currentQuestion = availableQuestions[0];
 
 function timer() {
   quiz();
@@ -19,10 +51,23 @@ function timer() {
 }
 
 function quiz() {
-  answerChoices.hidden = false;
+  answerChoicesContainer.hidden = false;
   startBtn.hidden = true;
   instructions.hidden = true;
-  questionEl.textContent = "Question 1";
+  questionEl.textContent = questions[0].question;
+  choices.forEach((choice) => {
+    var number = choice.dataset['number'];
+    choice.innerText = currentQuestion['choice' + number];
+  });
+  choices.addEventListener('click', selectedAnswer)
+  function selectedAnswer() {
+	  if (choices.innerText===currentQuestion[answer]){
+		  
+	  }
+
+  };
+
+  
 }
 
 startBtn.addEventListener("click", timer);
