2fcaf49bb75ae4ea3e35f299c205e845d2f6852e
camyacodes <camyacodes@gmail.com>
2021-11-15 11:21:03 -0500

    final code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,6 @@ var username = document.getElementById("username");
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 var highScoresList = document.getElementById("HighScoresList");
 
-
-
 saveHighScore = (e) => {
   e.preventDefault();
 
@@ -19,7 +17,7 @@ saveHighScore = (e) => {
   highScores.splice(5);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.replace('https://camyacodes.github.io/code-quiz/');
+  window.location.replace("https://camyacodes.github.io/code-quiz/");
 };
 
 viewScores = (e) => {
@@ -28,16 +26,16 @@ viewScores = (e) => {
   questionEl.textContent = "High Scores";
   var mainContent = document.getElementById("main-content");
   mainContent.hidden = true;
-  highScoresList.innerHTML = 
-  highScores.map((score) => {
+  highScoresList.innerHTML = highScores
+    .map((score) => {
       return `<li class="high-score">${score.name} - ${score.score}</li>`;
     })
     .join("");
-    var homeBtn = document.getElementById("homebtn")
-    homeBtn.hidden = false;
-    homeBtn.addEventListener('click', function(){
-      window.location.replace('https://camyacodes.github.io/code-quiz/');
-    })
+  var homeBtn = document.getElementById("homebtn");
+  homeBtn.hidden = false;
+  homeBtn.addEventListener("click", function () {
+    window.location.replace("https://camyacodes.github.io/code-quiz/");
+  });
 };
 
 var questions = [
@@ -48,49 +46,36 @@ var questions = [
   },
   {
     question: "The condition in an if/else statement is enclosed with _______.",
-    options: ["1. qoutes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+    options: [
+      "1. qoutes",
+      "2. curly brackets",
+      "3. parenthesis",
+      "4. square brackets",
+    ],
     answer: 2,
   },
   {
     question: "Arrays in JavaScript can be used to store _______.",
-    options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    options: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
+    ],
     answer: 3,
   },
   {
-    question: "String values must be enclosed within _______ when being assigned to variables",
+    question:
+      "String values must be enclosed within _______ when being assigned to variables",
     options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
     answer: 2,
   },
 ];
 
 
-// var questions = [
-//   {
-//     question: "What is 10/2?",
-//     options: ["3", "5", "115", "0"],
-//     answer: 0,
-//   },
-//   {
-//     question: "What is 20/2?",
-//     options: ["3", "5", "115", "0"],
-//     answer: 0,
-//   },
-//   {
-//     question: "What is 10/1?",
-//     options: ["3", "5", "115", "0"],
-//     answer: 0,
-//   },
-//   {
-//     question: "What is 3/1?",
-//     options: ["3", "5", "115", "0"],
-//     answer: 0,
-//   },
-// ];
-
-
 
 //define global variables that can be used in all of the functions
-var timeLeft = 2000;
+var timeLeft = 75;
 var currPosition = 0;
 
 var app = {
@@ -186,205 +171,3 @@ var app = {
 };
 app.start();
 
-// let x = 0;
-// var currentQuestion = {};
-// var availableQuestions = [...questions];
-// currentQuestion = availableQuestions[x];
-// var score = 0;
-
-// var initialTime = 75;
-// var timeLeft = initialTime;
-
-// function stopTimer() {
-//   clearInterval(timeInterval);
-// }
-
-// function timeInterval() {
-//   if (timeLeft > 0) {
-//     timerEl.textContent = "Time: " + timeLeft;
-//     timeLeft--;
-//   } else {
-//     timerEl.textContent = "0";
-//     stopTimer();
-//   }
-// }
-
-// function timer() {
-//   setInterval(timeInterval, 1000);
-//   quiz();
-
-// var timeInterval = setInterval(function () {
-//   if (timeLeft > 0) {
-//     timerEl.textContent = "Time: " + timeLeft;
-//     timeLeft = timeLeft - 1;
-//   } else {
-//     timerEl.textContent = "0";
-//     clearInterval(timeInterval);
-//   }
-// }, 1000);
-// }
-
-// function quiz() {
-//   startQuiz(currentQuestion);
-//   showChoices(currentQuestion);
-
-//   // x++;
-//   // choices.forEach((choice) => {
-//   //   var number = choice.dataset["number"];
-//   //   choice.innerText = currentQuestion["choice" + number];
-//   //   choice.addE                     ventListener("click", selectedAnswer);
-//   //   function selectedAnswer() {
-//   //     var answer = currentQuestion["answer"];
-//   //     if (choice.innerText === currentQuestion['choice' + answer]) {
-//   //       score = +10;
-//   //       console.log(score);
-//   //       choices.forEach((choice) => {
-//   //         currentQuestion = availableQuestions[x];
-//   //         var number = choice.dataset["number"];
-//   //         choice.innerText = currentQuestion["choice" + number];
-//   //       });
-//   //     }
-//   //   }
-//   // });
-// }
-
-// function startQuiz(currentQuestion) {
-//   answerChoicesEl.hidden = false;
-//   startBtn.hidden = true;
-//   instructions.hidden = true;
-//   questionEl.textContent = currentQuestion["question"];
-// }
-
-// function selectedAnswer() {
-//   var answer = currentQuestion["answer"];
-//   if (this.innerText === currentQuestion["choice" + answer]) {
-//     score++;
-//     console.log(score);
-//     next();
-//   } else if (this.innerText !== currentQuestion["choice" + answer]) {
-//   // console.log('Time Left before penalizing', timeLeft);
-//   timeLeft -= 10;
-//   // console.log('Time Left after penalizing', timeLeft);
-//     next();
-//   }
-// }
-// function showChoices(currentQuestion) {
-//   // console.log(currentQuestion);
-//   for (let choice of choices) {
-//     var number = choice.dataset["number"];
-//     choice.innerText = currentQuestion["choice" + number];
-//     choice.addEventListener("click", selectedAnswer);
-
-//     // choices.forEach((choice) => {
-//     //   var number = choice.dataset["number"];
-//     //   choice.innerText = currentQuestion["choice" + number];
-//     // choice.addEventListener("click", selectedAnswer);
-//     // function selectedAnswer() {
-//     //   var answer = currentQuestion["answer"];
-//     //   if (choice.innerText === currentQuestion["choice" + answer]) {
-//     //     score = +10;
-//     //     console.log(score);
-//     //     choices.forEach((choice) => {
-//     //       currentQuestion = availableQuestions[x];
-//     //       var number = choice.dataset["number"];
-//     //       choice.innerText = currentQuestion["choice" + number];
-//     //     });
-//     //   }
-//     // }
-//     // });
-//   }
-// }
-
-// // function showChoices(currentQuestion) {
-// //   console.log(currentQuestion);
-// //   for (let choice of choices) {
-// //     var number = choice.dataset["number"];
-// //     choice.innerText = currentQuestion["choice" + number];
-// //     choice.addEventListener("click", selectedAnswer);
-// //     function selectedAnswer() {
-// //       var answer = currentQuestion["answer"];
-// //       if (choice.innerText === currentQuestion["choice" + answer]) {
-// //         score++;
-// //         console.log(score);
-// //         next();
-// //       } else if (choice.innerText !== currentQuestion["choice" + answer]) {
-// //         console.log('Time Left before penalizing', timeLeft);
-// //         timeLeft -= 10;
-// //         timerEl.textContent = "Time: " + timeLeft;
-// //       console.log('Time Left after penalizing', timeLeft);
-// //         next();
-// //       }
-// //     }
-//     // choices.forEach((choice) => {
-//     //   var number = choice.dataset["number"];
-//     //   choice.innerText = currentQuestion["choice" + number];
-//     // choice.addEventListener("click", selectedAnswer);
-//     // function selectedAnswer() {
-//     //   var answer = currentQuestion["answer"];
-//     //   if (choice.innerText === currentQuestion["choice" + answer]) {
-//     //     score = +10;
-//     //     console.log(score);
-//     //     choices.forEach((choice) => {
-//     //       currentQuestion = availableQuestions[x];
-//     //       var number = choice.dataset["number"];
-//     //       choice.innerText = currentQuestion["choice" + number];
-//     //     });
-//     //   }
-//     // }
-//     // });
-// //   }
-// // }
-
-// // function userAnswer() {
-// //   for (let choice of choices) {
-// //     choice.addEventListener("click", selectedAnswer);
-// //     function selectedAnswer() {
-// //       var answer = currentQuestion["answer"];
-// //       if (choice.innerText === currentQuestion["choice" + answer]) {
-// //         score = +10;
-// //         console.log(score);
-// //       }
-// //       // else if (choice.innerText !== currentQuestion["choice" + answer]) {
-
-// //       // }
-
-// //       next();
-// //     }
-// //   }
-// // }
-
-// function next() {
-//   x++;
-//   var currentQuestion = {};
-//   var availableQuestions = [...questions];
-//   currentQuestion = availableQuestions[x];
-//   questionEl.textContent = currentQuestion["question"];
-//   if (currentQuestion == availableQuestions.length) {
-//     console.log("finished")
-//   }
-//   showChoices(currentQuestion)
-//   // console.log(currentQuestion);
-//   // if(x > 3) {
-//   //   // questionEl.textContent = currentQuestion["question"];
-//   //   // console.log(questionEl.innerText)
-//   //   console.log("home")
-//   //   // showChoices(currentQuestion);
-//   // } else {
-//   //   // console.log("home")
-//   //   showChoices(currentQuestion);
-//   // }
-//   // questionEl.textContent = currentQuestion["question"];
-//   // // console.log(questionEl.innerText)
-//   // showChoices(currentQuestion);
-
-// }
-
-// startBtn.addEventListener("click", timer);
-
-// // choices.onclick = () => {
-// // 	choices.forEach((choice) => {
-// // 		if (choice.innerText === currentQuestion[ 'choice' + 'answer'] ) {
-// // 			window.alert("It works!!")
-// // 		}
-
-// // }
